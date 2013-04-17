dd91b5a948af77e6ac71db572ff78c896466ef7d
Juan Reyero <juan@juanreyero.com>
2013-04-17 11:59:04 +0200

    Small doc change

--- org-jekyll.el
+++ org-jekyll.el
@@ -18,7 +18,7 @@
 ;;; exported files.  The title of the subtree is the title of the
 ;;; entry.  The title of the post is a link to the post's page.
 ;;;
-;;; Look at http://orgmode.org/worg/org-tutorials/org-jekyll.php for
+;;; Look at http://orgmode.org/worg/org-tutorials/org-jekyll.html for
 ;;; more info on how to integrate org-mode with Jekyll, and for the
 ;;; inspiration of the main function down there.
 ;;;
